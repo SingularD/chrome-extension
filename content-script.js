3eede041b6ef6ec774400df3b7c94da6ef3154ff
SingularD <906229740@qq.com>
2019-03-25 22:21:18 +0800

    完成对函数的封装，界面交互优化

--- content-script.js
+++ content-script.js
@@ -1,17 +1,20 @@
 class GetDom {
   constructor() {
-    this._element = null;
-    this._elementBackground = '';
-    this._id = '';
-    this._tab = null;
-    this._className = '';
-    this._origin = window
+    this._element = null; // 鼠标移动和点击时候被改变背景颜色的元素
+    this._elementBackground = ''; // 储存鼠标移动时候获取的元素本身的背景元素
+    this._id = ''; // 元素id
+    this._tab = null; // 爬虫展示框
+    this._hrefList = []; // 用于储存点击时候获取的超链接，临时变量，之后会转化为字符串
     this._msg = {
       'className': '',
       'id': ''
-    };
+    }; // 发送给后台的json
 
   }
+
+  /**
+   *  初始化元素，添加显示框
+   */
   _intiTab() {
 
     window.addEventListener('DOMContentLoaded', () => {
@@ -22,13 +25,13 @@ class GetDom {
       tab.innerHTML =
         `
         <div class="tab-title" style="width: 100%;height: 20px;background-color: #f3f3f3;border-bottom: #d0d0d0 1px solid">
-            <p class="tab-title-text" style="line-height: 20px">欢迎使用晴空爬虫系统</p>
+            <p class="tab-title-text" style="margin-left:50px;line-height: 20px">欢迎使用晴空爬虫系统</p>
         </div>
-        <div class="tab-left" style="width: 70%;height: 100%;border-right: #d0d0d0 solid 1px;float: left">
-            <h1 class="tab-left-content" style="font-size: 15px;line-height: 15px;">这里将显示dom元素</h1>
+        <div class="tab-left" style="width: 49%;height: 100%;border-right: #d0d0d0 solid 1px;float: left;overflow: auto">
+            <h1 class="tab-left-content" style="margin-left:50px;font-size: 15px;line-height: 15px;">这里将显示dom元素</h1>
         </div>
-        <div class="tab-right" style="width: 29%;height: 100%; float: left">
-            <h1 class="tab-right-options">这里将显示选项</h1>
+        <div class="tab-right" style="width: 50%;height: 100%; float: left;overflow: auto">
+            <h1 class="tab-right-content" style="margin-left: 30px">点击获取DOM元素</h1>
         </div>
         `
       tab.style.cssText =
@@ -47,15 +50,19 @@ class GetDom {
       this._tab = tab
     })
   }
+
+  /**
+   * 全局监听鼠标移动事件，改变鼠标所指元素的背景颜色
+   */
   changeBackgroundColor() {
-    this._origin.addEventListener('mouseover', (event) => {
+    window.addEventListener('mouseover', (event) => {
       if (event.target.className === 'tab'
         || event.target.className === 'tab-title'
         || event.target.className === 'tab-title-text'
         || event.target.className === 'tab-left'
         || event.target.className === 'tab-left-content'
         || event.target.className === 'tab-right'
-        || event.target.className === 'tab-right-options'
+        || event.target.className === 'tab-right-content'
       ) return
       this._element = event.target
       this._elementBackground = this._element.style.backgroundColor
@@ -63,11 +70,19 @@ class GetDom {
       this.showDom(this._element)
     })
   }
+
+  /**
+   * 全局监听，鼠标移开时回复背景颜色
+   */
   restoreBackgroundColor() {
-    this._origin.addEventListener('mouseout', () => {
+    window.addEventListener('mouseout', () => {
       this._element.style.backgroundColor = this._elementBackground
     })
   }
+
+  /**
+   * 全局监听点击事件，获取触发事件的id
+   */
   getId() {
     window.addEventListener('click', (event) => {
       this._id = event.target.id
@@ -75,6 +90,12 @@ class GetDom {
       event.preventDefault()
     })
   }
+
+  /**
+   * 左边显示框
+   * 显示输入元素的标签名，类名，id，标签内的值以及媒体链接（src）
+   * @param dom
+   */
   showDom(dom) {
     document.querySelector('.tab-left-content').innerText =
       `
@@ -82,39 +103,94 @@ class GetDom {
         ${dom.innerText ? '标签内的值为：' + dom.innerText : '媒体链接：' + dom.src}
       `
   }
+
+  /**
+   * 右边显示框
+   * 点击事件触发时对应元素的标签名，类名，id，标签内的值以及媒体链接（src）
+   * @param dom
+   */
+  confirmDom(dom) {
+    document.querySelector('.tab-right-content').innerText =
+      `
+        标签名：${dom.tagName}\t类名：${dom.className}\t${dom.id? +'id:'+dom.id : ''}\n
+        ${dom.innerText ? '标签内的值为：' + dom.innerText : '媒体链接：' + dom.src}\n
+        ${this.getHref(dom)}
+      `
+  }
+
+  /**
+   * 改变原来网页的高度，使得爬虫信息显示框拉到最底部是不会覆盖原来网页的内容
+   */
+  addBodyHeight() {
+    window.onload = () => {
+      document.body.style.height = document.querySelector('body').offsetHeight + 600 + 'px'
+      console.log(document.querySelector('body').offsetHeight)
+    }
+  }
+
+  /**
+   * 获得输入元素或者子元素a标签链接（href）
+   * @param dom
+   * @returns {string} 返回一个字符串，为所有输入元素下的href
+   */
+  getHref(dom) {
+    this._hrefList = []
+    let count = 1;
+    if (dom.getElementsByTagName('a').length > 0) {
+      console.log(dom.getElementsByTagName('a'))
+
+      for (let x of dom.getElementsByTagName('a')) {
+        let singleHref = `超链接${count++}为: ${x.href}\n`
+        this._hrefList.push(singleHref)
+      }
+    }
+    return this._hrefList.join('')
+  }
+
+  /**
+   * 全局监听点击事件，获取点击元素的类名
+   */
   getClassName() {
     window.addEventListener('click', (event) => {
-      for (let x in this._tab.children) {
-        console.log(x.innerHTML)
-      }
-      this._className = event.target.className
-      console.log(`类名: ${this._className}`)
+      this.confirmDom(this._element)
       event.preventDefault()
     })
   }
-  sendMessage() {
-    chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-      console.log(response.farewell);
-    });
+
+  /**
+   * 接受popup.js消息，'start'开始爬虫模式，'end'关闭爬虫模式，并重新刷新网页
+   */
+  receiveMessage() {
+    const _this = this
+    chrome.runtime.onMessage.addListener(
+      (request, sender, sendResponse) => {
+        if (request.state === "start") {
+          alert(request.value)
+          _this.show()
+          _this.changeBackgroundColor();
+          _this.restoreBackgroundColor()
+          _this.getClassName()
+          _this.getId()
+          sendResponse({state: 'start'})
+        }else {
+          document.body.removeChild(this._tab)
+          alert('爬虫模式即将关闭！')
+          window.location.reload()
+        }
+      });
   }
+
+  /**
+   * 爬虫信息显示框动画
+   */
   show() {
     this._tab.style.transform = 'scale(1,1)'
   }
 }
 const target = new GetDom()
-target._intiTab()
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.cmd === "test")
-      sendResponse({ok: "start"});
-    alert('aaa')
-    target.show()
-    target.changeBackgroundColor();
-    target.restoreBackgroundColor()
-    target.getClassName()
-    target.getId()
-    // target.sendMessage()
-  });
+target._intiTab();
+target.addBodyHeight();
+target.receiveMessage();
 
 
 
--- popup.js
+++ popup.js
@@ -1,18 +1,44 @@
-document.querySelector('.start').addEventListener('click', () => {
-  send({cmd:'test', value:'你好，我是popup！'}, function(response)
-  {
-    alert('bbb')
-    console.log('来自content的回复：'+response);
-  })
-})
-
-const send = (msg, callback) => {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
-  {
-    chrome.tabs.sendMessage(tabs[0].id, msg, function(response)
+class Popup{
+  /**
+   * 封装chrome插件js文件之间的通信
+   * @param msg 需要发送的信息
+   * @param callback 发送成功后的回调函数
+   */
+  send(msg, callback) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
     {
-      if(callback) callback(response);
+      chrome.tabs.sendMessage(tabs[0].id, msg, function(response)
+      {
+        if(callback) callback(response);
+      });
     });
-  });
+  }
+
+  /**
+   * 向content-script.js页面发送消息
+   * @param state 插件点击的按钮
+   * @param value 点击按钮代表的值，会在页面中以弹出框的形式显示
+   */
+  sendInfo(state,value) {
+    this.send({state: state, value: value}, (response) => {
+      console.log('来自content的回复：' + response);
+    })
+  }
+
+  /**
+   * 监听点击事件，发送按钮对应的值给content-script.js
+   */
+  judge() {
+    window.addEventListener('click', (event) => {
+      if (event.target.className === 'btn start') {
+        this.sendInfo('start','开启爬虫模式~')
+      } else {
+        this.sendInfo('end','关闭爬虫模式~')
+      }
+    })
+  }
 }
+const popup = new Popup()
+popup.judge()
+
 
